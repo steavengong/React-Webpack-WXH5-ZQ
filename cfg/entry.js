d397efe3fb7bdb05995c6e0ceaf96d3897431dca
steavengong <842228597@qq.com>
2016-09-01 16:21:01 +0800

    1

---
+++ cfg/entry.js
@@ -0,0 +1,21 @@
+/**
+ * Created by Administrator on 2016/8/12.
+ */
+const defaultSettings = require('./defaults');
+const entry = {}
+entry.setEntryFile = function(){
+  defaultSettings.enterFileName = 'index';
+}
+
+entry.setPublicPath = function(validEnv){
+  switch (validEnv){
+    case 'dist':
+      defaultSettings.publicPath = "http://www.mengbp.com/web/WXH5/MBP/WebpackTest/assets/";
+      break;
+    default:
+      defaultSettings.publicPath = "/assets/";
+      break;
+  }
+}
+
+module.exports = entry;
